7dba1295903997ae6df38f2b6c43f8f21df9d018
betulgecer <gecer.b13@gmail.com>
2022-01-19 00:09:32 +0300

    10.1

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -26,7 +26,5 @@ namespace Business.Constants
         public static string ColorListed = "Color Listed!";
         public static string ColorDeleted = "Color Deleted!";
         public static string ColorUpdated = "Color Updated!";
-
-        //for the
     }
 }
